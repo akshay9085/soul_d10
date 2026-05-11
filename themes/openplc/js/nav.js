8ff060b7d07dc7e72bd4aafb42d2b3e7fd0bb27a
akshay9085 <akshaythakur9085@gmail.com>
2026-05-11 14:35:53 +0530

    theme updated

--- themes/openplc/js/nav.js
+++
@@ -1,5 +0,0 @@
-// $( '#openplc_nav .navbar-nav a' ).on( 'click', function () {
-// 	$( '#openplc_nav .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
-// 	$( this ).parent( 'li' ).addClass( 'active' );
-// // });
-
